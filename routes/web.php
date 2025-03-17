12889674fe73d5d0c61bc1b79d4574b92fc679c1
Levi-Schmidt <levi.oliveira.schmidt@gmail.com>
2025-03-17 11:59:30 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rota para verificar se as tabelas foram criadas
+Route::get('/check-tables', function () {
+    $tables = DB::select('SHOW TABLES');
+    return response()->json($tables);
 });
