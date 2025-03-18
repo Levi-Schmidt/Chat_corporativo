1e092bc082bcae5ba41704756317f156093c2294
Levi-Schmidt <levi.oliveira.schmidt@gmail.com>
2025-03-18 14:35:49 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 // Rota para verificar se as tabelas foram criadas
