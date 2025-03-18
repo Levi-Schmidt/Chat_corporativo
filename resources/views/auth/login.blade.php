e830354c742bff53fbb80008a455c99c28230236
Levi-Schmidt <levi.oliveira.schmidt@gmail.com>
2025-03-18 14:42:03 -0300

    Delete resources/views/auth directory

--- resources/views/auth/login.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    <input type="email" name="email" placeholder="E-mail" required>
-    <input type="password" name="password" placeholder="Senha" required>
-    <button type="submit">Entrar</button>
-</form>
--- resources/views/auth/register.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('register') }}" method="POST">
-    @csrf
-    <input type="text" name="name" placeholder="Nome" required>
-    <input type="email" name="email" placeholder="E-mail" required>
-    <input type="password" name="password" placeholder="Senha" required>
-    <input type="password" name="password_confirmation" placeholder="Confirme sua senha" required>
-    <button type="submit">Registrar</button>
-</form>
